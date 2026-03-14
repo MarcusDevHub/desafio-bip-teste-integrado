73136b8bcce10d9e3c7e4c74f4a0cbe156790115
MarcusDevHub <marcusthuliok9@gmail.com>
2026-03-14 20:30:18 -0300

    test(backend): adiciona teste de busca de beneficio por id

--- backend-module/src/test/main/java/com/example/backend/BeneficioControllerTest.java
+++ backend-module/src/test/main/java/com/example/backend/BeneficioControllerTest.java
@@ -29,7 +29,7 @@ class BeneficioControllerTest {
     @Test
     void deveListarBeneficios() throws Exception {
         Beneficio beneficio1 = new Beneficio();
-        beneficio1.setId(6);
+        beneficio1.setId(6L);
         beneficio1.setNome("Info Falsa1");
         beneficio1.setDescricao("Benefício A");
         beneficio1.setValor(new BigDecimal("500.00"));
@@ -47,7 +47,7 @@ class BeneficioControllerTest {
         mockMvc.perform(get("/api/v1/beneficios"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$[0].id").value(6))
+                .andExpect(jsonPath("$[0].id").value(6L))
                 .andExpect(jsonPath("$[0].nome").value("Info Falsa1"))
                 .andExpect(jsonPath("$[0].descricao").value("Benefício A"))
                 .andExpect(jsonPath("$[0].valor").value(500.00))
@@ -57,6 +57,28 @@ class BeneficioControllerTest {
                 .andExpect(jsonPath("$[1].descricao").value("Benefício B"))
                 .andExpect(jsonPath("$[1].valor").value(300.00))
                 .andExpect(jsonPath("$[1].ativo").value(true));
+
+    }
+
+    @Test
+    void deveBuscarBeneficioPorId() throws Exception {
+        Beneficio beneficio = new Beneficio();
+        beneficio.setId(6L);
+        beneficio.setNome("Info Falsa1");
+        beneficio.setDescricao("Benefício A");
+        beneficio.setValor(new BigDecimal("500.00"));
+        beneficio.setAtivo(true);
+
+        when(service.buscarPorId(6)).thenReturn(beneficio);
+
+        mockMvc.perform(get("/api/v1/beneficios/{id}", 6L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.nome").value("Info Falsa1"))
+                .andExpect(jsonPath("$.descricao").value("Benefício A"))
+                .andExpect(jsonPath("$.ativo").value(true));
+
+        when(service.buscarPorId(6L)).thenReturn(beneficio);
 
     }
 
